2c4c485c49f2f0212638ff1cb80a4c1a9a475385
Mike Thacker <michaeljthacker@gmail.com>
2024-10-13 16:23:22 -0700

    Fixing messed up game-screen

--- js/Field.js
+++ js/Field.js
@@ -11,23 +11,23 @@ export class Field {
         const field = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         field.setAttribute('x', '0');
         field.setAttribute('y', '0');
-        field.setAttribute('width', this.fieldLength);
-        field.setAttribute('height', this.fieldWidth);
+        field.setAttribute('width', '100%'); // Use relative units
+        field.setAttribute('height', '100%'); // Use relative units
         field.setAttribute('class', 'field-background');
         this.soccerField.appendChild(field);
 
         const centerLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-        centerLine.setAttribute('x1', this.fieldLength / 2);
+        centerLine.setAttribute('x1', '50%'); // Use relative units
         centerLine.setAttribute('y1', '0');
-        centerLine.setAttribute('x2', this.fieldLength / 2);
-        centerLine.setAttribute('y2', this.fieldWidth);
+        centerLine.setAttribute('x2', '50%'); // Use relative units
+        centerLine.setAttribute('y2', '100%'); // Use relative units
         centerLine.setAttribute('class', 'field-line');
         this.soccerField.appendChild(centerLine);
 
         const centerCircle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        centerCircle.setAttribute('cx', this.fieldLength / 2);
-        centerCircle.setAttribute('cy', this.fieldWidth / 2);
-        centerCircle.setAttribute('r', 5);
+        centerCircle.setAttribute('cx', '50%'); // Use relative units
+        centerCircle.setAttribute('cy', '50%'); // Use relative units
+        centerCircle.setAttribute('r', '5%'); // Use relative units
         centerCircle.setAttribute('class', 'field-line');
         centerCircle.setAttribute('fill', 'none');
         this.soccerField.appendChild(centerCircle);
@@ -36,10 +36,10 @@ export class Field {
     // Method to create a goal on the field
     createGoal(x, y, width, height) {
         const goal = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-        goal.setAttribute('x', x);
-        goal.setAttribute('y', y);
-        goal.setAttribute('width', width);
-        goal.setAttribute('height', height);
+        goal.setAttribute('x', `${(x / this.fieldLength) * 100}%`); // Scale to fit container
+        goal.setAttribute('y', `${(y / this.fieldWidth) * 100}%`); // Scale to fit container
+        goal.setAttribute('width', `${(width / this.fieldLength) * 100}%`); // Scale to fit container
+        goal.setAttribute('height', `${(height / this.fieldWidth) * 100}%`); // Scale to fit container
         goal.setAttribute('class', 'goal');
         this.soccerField.appendChild(goal);
     }
@@ -49,10 +49,10 @@ export class Field {
         for (let x = 0; x < this.fieldLength; x += this.cellSize) {
             for (let y = 0; y < this.fieldWidth; y += this.cellSize) {
                 const cell = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-                cell.setAttribute('x', x);
-                cell.setAttribute('y', y);
-                cell.setAttribute('width', this.cellSize);
-                cell.setAttribute('height', this.cellSize);
+                cell.setAttribute('x', `${(x / this.fieldLength) * 100}%`); // Scale to fit container
+                cell.setAttribute('y', `${(y / this.fieldWidth) * 100}%`); // Scale to fit container
+                cell.setAttribute('width', `${(this.cellSize / this.fieldLength) * 100}%`); // Scale to fit container
+                cell.setAttribute('height', `${(this.cellSize / this.fieldWidth) * 100}%`); // Scale to fit container
                 cell.setAttribute('class', 'grid-cell');
                 cell.addEventListener('click', () => handleCellClick(x, y));
                 this.soccerField.appendChild(cell);
